ee6f2d0db47f1735100826b36cd03d416bd7ebc4
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-10-28 17:32:08 -0400

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Mami Ravaloarison on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
