01693405f0c86cf8795f3686b0ff8ed8ea564961
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-10-30 10:37:19 -0400

    creaturedetail added

---
+++ Pokemon/Creature.swift
@@ -0,0 +1,8 @@
+//
+//  Creature.swift
+//  Pokemon
+//
+//  Created by Mami Ravaloarison on 10/29/24.
+//
+
+import Foundation
---
+++ Pokemon/CreatureDetail.swift
@@ -0,0 +1,8 @@
+//
+//  CreatureDetail.swift
+//  Pokemon
+//
+//  Created by Mami Ravaloarison on 10/29/24.
+//
+
+import Foundation
